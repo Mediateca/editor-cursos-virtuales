68007a23b5814e47f116b22f4bdb3c4906105553
Oliver Castelblanco Martínez <ocastelblanco@gmail.com>
2018-09-19 10:49:47 -0500

    Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuillEditorComponent } from 'ngx-quill/src/quill-editor.component';
 
+import Quill from 'quill';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'editorCV';
+export class AppComponent implements OnInit {
+    texto: String = "";
+    configuracionTexto = {toolbar: [
+            ['bold', 'italic', 'underline', 'strike','blockquote'],
+            ['sub', 'sup'],
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+            ['clean'],                                         // remove formatting button
+
+            ['link', 'image', 'video']                         // link and image, video
+        ]
+    };
+    constructor() {}
+    ngOnInit () {
+
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { QuillModule } from 'ngx-quill';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    ClarityModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        ClarityModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        QuillModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
